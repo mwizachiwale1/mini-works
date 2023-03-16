63c8e6568fec8ba886bb4441676acbc25422254b
mwizachiwale1 <47919799+mwizachiwale1@users.noreply.github.com>
2023-03-16 11:12:32 +0200

    Update create_groups.py
    Added more flexibility to group creation

--- create_groups/create_groups.py
+++ create_groups/create_groups.py
@@ -5,9 +5,10 @@
 # please pip install 'pandas' and 'xlswriter' to run the program. 
 # THANK YOU
 
+import math
 import pandas as pd
-students = pd.read_csv("students.csv")
-file_name = "studentgroups5.xlsx" # name of the file to save the group lists in
+students = pd.read_csv("student_list.csv")
+file_name = "generatedgroups7.xlsx" # name of the file to save the group lists in
 student_data = students[["Full name", "Computer number"]] # get only the necessary columns
 
 # shuffle the rows
@@ -18,7 +19,7 @@ shuffled_student_data = shuffled_student_data.to_dict()
 
 number_of_students = len(shuffled_student_data["Full name"])
 students_per_group = 6
-number_of_groups = int(number_of_students/students_per_group)
+number_of_groups = math.ceil(number_of_students/students_per_group)
 
 group_data = {} # stores the students in a group
 i = 0
@@ -26,15 +27,19 @@ count = 0 # keep track of current row number in shuffled_student_data
 
 # create writer object to enable editing the excel sheet
 writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+print(number_of_groups)
 while i < number_of_groups:
     col_name = f"Group_{i+1}"
     k = 0
     temp = {} # store temporal entries
     
     while k < 6:
-        temp[k] = {shuffled_student_data['Full name'][count], shuffled_student_data['Computer number'][count]}
-        k += 1
-        count += 1
+        if count < number_of_students:
+            temp[k] = {shuffled_student_data['Full name'][count], shuffled_student_data['Computer number'][count]}
+            k += 1
+            count += 1
+        else:
+            break
 
     group_data[col_name] = temp
     group_data_frame =  pd.DataFrame(group_data) # convert to dataframe
